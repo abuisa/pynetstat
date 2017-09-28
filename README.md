dfa2c1394a3e2557e2dffadabc5647043f5997fe
elyas <elyas.uii14@gmail.com>
2017-09-28 18:34:39 +0700

    Fix output and Readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
+## TENTANG TOOL INI 
+Tool/Script ini adlah tool untuk melihat aktifitas koneksi dan port yang terbuka menggunakan netstat 
+yang dieksekusi menggunakan python untuk mendapatkan format output yang sesuai dengan keinginan, 
+dilengkapi dengan filter yang lebih memudahkan
 
+## KETERANGAN FILE dan HOW TO 
+Terdapat dua file `.py` : 
+	1. `getappname.py` untuk menampilkan aktifitas koneksi dan port yang digunakan, 
+		HOW TO : 
+		tulis perintah di terminal/CMD : `python getappname.py` lalu ENTER
+		![Tampilah Awal](img_exp/exp0.JPG "Tampilan Awal")
+		langsung tekan ENTER untuk menampilkan semua atau tulis string untuk filter output
+		berikut contoh hasil screenshot menggunakan filter : 
+		![Filter Contoh Awal](img_exp/exp1.JPG "Filter Contoh Awal")
+	2. `getappname-table-output.py` untuk menampilkan aktifitas koneksi dan port yang digunakan dengan output dalam format tabel,
+		HOW TO :
+		pertama, install dulu module `terminaltables` dengan perintah : `pip install terminaltables`
+		setelah itu perintah sama dengan perintah pada `getappname.py`
+## CONTOH OUTPUT 
+	1. Contoh kesatu : 
+	![Tampilah satu](img_exp/exp0.JPG "Tampilan satu"
+	2. Contoh kedua : 
+	![Tampilah dua](img_exp/exp1.JPG "Tampilan dua"
+	3. Contoh ketiga : 
+	![Tampilah tiga](img_exp/exp2.JPG "Tampilan tiga"
--- getappname-table-output.py
+++ getappname-table-output.py
@@ -2,14 +2,6 @@ import subprocess
 import psutil
 import csv
 
-ket= '''-------------------------------------------------
- * netstat untuk menampilkan applikasi yang sedang menggunakan port
- * hasilnya sama dengan <netstat -ab> tapi beda format output dan tidak ada waktu tunggu
- * output bisa difilter 
- * Format Output : 
-   [1]No. [2]Proto  [3]Local Address  [4]Foreign Address  [5]State  [6]PID  [7]AppName
--------------------------------------------------
-'''
 
 def get_appname(pid):
 	ps = psutil.Process(int(pid))
@@ -20,15 +12,16 @@ def w2file(fl,s):
 		f = open(fl,'w')
 		f.write(s+"\n")
 	except:
-		exit()
+		#f = open(fl,'w')
 	f.close
-	
+
+batas = '------------------------------------------------------------------'		
 cmd_out = subprocess.Popen(['netstat', '-ano'], stdout=subprocess.PIPE).communicate()
 out_tolist = list(cmd_out)
 
 for out_line in out_tolist:
 	if out_line != None:
-		out_line = out_line.decode('utf-8') 
+		out_line = out_line.decode('utf-8') # decode, untuk python 3
 		w2file('NetStat.out',out_line)
 
 	
@@ -50,19 +43,15 @@ def output_v1():
 					pid = f_line[-1]
 					exe = get_appname(pid)
 					p_out = str(ix)+'.'+s+ pro +s+ sta +s+loa +s+s+  foa +s+ pid +s+ exe
-					#p_out = '\t'.join(f_line)
 					print (p_out)
 				except Exception as e:
 					print (e)
-				
+				#print('-------------------------------------------------')
 
-				
 def output_v2(str_filter):
 	no = 0
 	f = open("NetStat.out")
 	csv_f = csv.reader(f)
-	#hdr_s = ["NO","PROTO","LOCAL_IP","FOREIGN_IP","STATE","PID","APP_NAME"]
-	#print ('\t'.join(hdr_s))
 	for row in csv_f:
 		if row:
 			row_tol = row[0].split()
@@ -70,23 +59,26 @@ def output_v2(str_filter):
 			pid = row_tol[-1]
 			if pro == "TCP" or pro == "UDP":
 				exe = get_appname(pid)
-				row = row[0]				
+				row = row[0]
 				if any(str_filter.upper() in el_row.upper() for el_row in row_tol) or (str_filter.upper() in exe.upper())  and str_filter != '':
-					no = no + 1					
+					no = no + 1
 					print(' '+str(no)+'.\t'+ row + '\t' +exe)
-				elif str_filter == '':
+				if str_filter == '':
 					no = no + 1
 					print(' '+str(no)+'.\t'+ row + '\t' +exe)				
-				#else:
-				#	print(' Filter Not Found !')
 
 
 
+print ('''-------------------------------------------------
+ * netstat untuk menampilkan applikasi yang sedang menggunakan port
+ * hasilnya sama dengan <netstat -ab> tapi beda format output dan tidak ada waktu tunggu
+ * output bisa difilter 
+-------------------------------------------------
+''')
 while True:
-	try:	
-		print(ket) # beri tanda pagar jika tidak ingin tampilkan baris ini 
+	output_v2('')
+	try:		
 		str_filter = input('\n Masukkan String untuk Filter output, [x/X for Exit] : ')
-		print('------------------------------------------------------------')
 		if str_filter == 'x' or str_filter == 'X':
 			exit()
 		#output_v1()
